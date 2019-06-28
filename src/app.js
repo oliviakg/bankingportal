158d54bbbc74ea3945c256e638d1ed622b378e81
Olivia Gosselin <oliviakg@umich.edu>
2019-06-28 08:54:11 -0400

    using express router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
+
 const app = express();
+
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
+
+const { accounts, users, writeJSON } = require('./data');
 
 const port = process.env.PORT || 3000;
 
@@ -12,59 +21,14 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true}));
 
-const accountData = fs.readFileSync(
-    path.join(__dirname, 'json', 'accounts.json'), 'utf8'
-);
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(
-    path.join(__dirname, 'json', 'users.json'), 'utf8'
-)
-const users = JSON.parse(userData);
-
 app.get('/', (req, res) => {
     res.render('index', { title: 'Account Summary', accounts});
 });
-
-app.get('/savings', (req, res) => {
-    res.render('account', { account: accounts.savings})
-})
-
-app.get('/checking', (req, res) => {
-    res.render('account', { account: accounts.checking})
-})
-
-app.get('/credit', (req, res) => {
-    res.render('account', { account: accounts.credit})
-})
 
 app.get('/profile', (req, res) => {
     res.render('profile', { user: users[0]})
 })
 
-app.get('/transfer', (req, res) => {
-    res.render('transfer')
-})
-
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-    const accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
-    res.render('transfer', {message: 'Transfer Completed'})
-})
-
-app.get('/payment', (req, res) => {
-    res.render('payment', { account: accounts.credit})
-})
-
-app.post('/payment', (req, res) => {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount, 10);
-    const accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
-    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
-})
 
 app.listen(port, () => {
     console.log('PS Project Running on Port 3000!')
